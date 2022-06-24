062fc9a14de9b755efa29bbfe15de13ccbda7019
andude10 <indko.06@gmail.com>
2022-06-24 21:20:35 +0300

    refactor

--- wiki.py
+++ wiki.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python
 
 import argparse
-import wikipedia
-import time
-import threading
 import concurrent.futures
+import threading
 
-from rich.columns import Columns
+import wikipedia
 from rich import print, pretty
-from rich.pretty import Pretty
+from rich.columns import Columns
 from rich.panel import Panel
-from rich.progress import track
-from rich.progress import Progress
+from rich.pretty import Pretty
 
 pretty.install()
 thread_local = threading.local()
 
+
 # sub-command functions
 def search(args):
     kwards = args._get_kwargs()
-    search_quary = kwards.__getitem__(0).__getitem__(1)
-    isAll = kwards.__getitem__(1).__getitem__(1)
+    search_query = kwards.__getitem__(0).__getitem__(1)
+    is_all = kwards.__getitem__(1).__getitem__(1)
 
-    results = get_search_results(search_quary, display_All=isAll)
+    results = get_search_results(search_query, display_all=is_all)
 
     print('There what we found:')
     print(results)
+
 
 def open_page(args):
     page_title = args._get_kwargs().__getitem__(0).__getitem__(1)
@@ -34,21 +33,21 @@ def open_page(args):
     result = wikipedia.page(title=page_title)
     print(result)
 
-# utility functions
-def get_search_results(search_quary, display_All=False):
-    pages = wikipedia.search(search_quary)
 
-    if display_All:
+# utility functions
+def get_search_results(search_query, display_all=False):
+    pages = wikipedia.search(search_query)
+
+    if display_all:
         with concurrent.futures.ProcessPoolExecutor() as executor:
             futures = [executor.submit(get_page_summary, page) for page in pages]
-        
-        print('все ок')
-        
+
         pages_renderables = [Panel(f.result()) for f in concurrent.futures.as_completed(futures)]
 
     else:
         pages_renderables = [Pretty(page) for page in pages]
     return Columns(pages_renderables)
+
 
 def get_page_summary(page):
     title = page
